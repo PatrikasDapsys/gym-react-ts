91b846aeeb8fce4ec2aeeba8eaf64556a63d9ab1
PatrikasDapsys <patrikas.dapsys@gmail.com>
2023-05-20 21:44:11 +0300

    benefits page complete

---
+++ src/scenes/benefits/index.tsx
@@ -0,0 +1,169 @@
+import { BenefitType, SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import {
+  HomeModernIcon,
+  UserGroupIcon,
+  AcademicCapIcon,
+} from "@heroicons/react/24/solid";
+import HText from "@/shared/HText";
+import Benefit from "./Benefit";
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
+import ActionButton from "@/shared/ActionButton";
+
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6" />,
+    title: "State Of The Art Facilities",
+    description:
+      "Indulge in an extraordinary fitness journey with our cutting-edge, state-of-the-art facilities and elevate your workout experience to new heights.",
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "100's of Diverse Classes",
+    description:
+      "Embark on a fitness adventure like no other, with our vast selection of 100's of diverse classes that cater to every workout preference and unleash your full potential.",
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6" />,
+    title: "Expert and Professional trainers",
+    description:
+      "Elevate your fitness journey with our expert and professional trainers, who provide personalized guidance and unwavering support.",
+  },
+];
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 },
+  },
+};
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+function Benefits({ setSelectedPage }: Props) {
+  return (
+    <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      >
+        {/* HEADER */}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          className="md:my-5 md:w-3/5"
+        >
+          <HText>MORE THAN JUST A GYM</HText>
+          <p className="my-5 text-sm">
+            We provide world class fitness equipment, trainers and classes to
+            get you to your ultimate fitness goals with ease. We provide true
+            care into each and every member.
+          </p>
+        </motion.div>
+        {/* BENEFITS */}
+
+        <motion.div
+          className="mt-5 items-center justify-between gap-8 md:flex"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+        >
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit
+              key={benefit.title}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+              setSelectedPage={setSelectedPage}
+            />
+          ))}
+        </motion.div>
+
+        {/* GRAPHICS AND DESCRIPTION */}
+
+        <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+          {/* GRAPHIC */}
+          <img
+            className="mx-auto"
+            src={BenefitsPageGraphic}
+            alt="benefits-page-grapthic"
+          />
+          {/* DESCRIPTION */}
+
+          <div className="">
+            {/* TITLE */}
+            <div className="relative">
+              <div className="before:absolute before:-left-20 before:-top-20 before:z-[1] before:content-abstractwaves">
+                <motion.div
+                  className=""
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ duration: 0.5 }}
+                  variants={{
+                    hidden: { opacity: 0, x: 50 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                >
+                  <HText>
+                    MILLIONS OF HAPPY MEMBERS GETTING{" "}
+                    <span className="text-primary-500">FIT</span>
+                  </HText>
+                </motion.div>
+              </div>
+            </div>
+
+            {/* DESCRIPTION */}
+
+            <motion.div
+              className=""
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: 100 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
+              <p className="my-5">
+                Step into a vibrant fitness community where millions of happy
+                members are rewriting their stories of transformation. With
+                unwavering dedication and personalized workouts, our members are
+                achieving remarkable results and unlocking their full potential.
+                Join us today to experience the power of motivation,
+                camaraderie, and support that permeates our inclusive
+                environment.
+              </p>
+              <p className="mb-5">
+                Join our thriving gym community, where fitness enthusiasts of
+                all levels come together to inspire and uplift. Embark on your
+                transformative journey with millions of happy members towards a
+                healthier, fitter future.
+              </p>
+            </motion.div>
+
+            {/* BUTTON  */}
+            <div className="relative mt-16">
+              <div className="before:-bottum-20 before:absolute before:right-40 before:z-[-1] before:content-sparkles">
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Join Now
+                </ActionButton>
+              </div>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </section>
+  );
+}
+
+export default Benefits;
